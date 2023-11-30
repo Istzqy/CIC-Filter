f87ffeefb470695a2adcff2f508fb2d62014a473
Istzqy <zqyaigy@foxmail.com>
2023-11-30 22:42:46 +0800

    1130

--- CIC_Filter.m
+++ CIC_Filter.m
@@ -58,6 +58,82 @@ plot(f,20*log10(h2));
 xlabel('Normalized Frequency(cycles/sample)');
 ylabel('Magnitude(dB)');
 
+%% CIC滤波器
+%Z域传递函数： H(z) =[(1-z^-10) / (1 - z^-1)]^Q 
+b = [1 0 0 0 0 0 0 0 0 0 -1];
+a = [1 -1];
+figure(1);
+zplane(b,a);
+
+% 数字滤波器频率响应
+[h,w] = freqz(b,a,1024,'whole');
+% 没理解这行代码的实际物理意义
+h(1,1) = length(b)-1;
+% 平移零频分量，然后绘制以零为中心的频谱
+h_shift = fftshift(h);
+n = length(w);
+% zero-centered frequency range
+w_shift = (-n/2:n/2-1)*(1/n); 
+% plot(w_shift,abs(h_shift))
+
+%观察级联个数对旁瓣衰减的影响
+figure(2);
+% 多少个级联
+Q = 1; 
+subplot(2,2,1);
+plot(w_shift,20*log10(abs(h_shift).^Q))
+peaks = findpeaks(20*log10(abs(h_shift).^Q));
+xlabel('Normalized frequency(cycles/sample)');
+ylabel('|H(e^j^\omega))|(dB)');
+title('Q=1');
+grid on;
+Q = 2; 
+subplot(2,2,2);
+plot(w_shift,20*log10(abs(h_shift).^Q))
+peaks = findpeaks(20*log10(abs(h_shift).^Q));
+xlabel('Normalized frequency(cycles/sample)');
+ylabel('|H(e^j^\omega))|(dB)');
+title('Q=2');
+grid on;
+Q = 3; 
+subplot(2,2,3);
+plot(w_shift,20*log10(abs(h_shift).^Q))
+peaks = findpeaks(20*log10(abs(h_shift).^Q));
+xlabel('Normalized frequency(cycles/sample)');
+ylabel('|H(e^j^\omega))|(dB)');
+title('Q=3');
+grid on;
+Q = 4; 
+subplot(2,2,4);
+plot(w_shift,20*log10(abs(h_shift).^Q))
+peaks = findpeaks(20*log10(abs(h_shift).^Q));
+xlabel('Normalized frequency(cycles/sample)');
+ylabel('|H(e^j^\omega))|(dB)');
+title('Q=4');
+grid on;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
